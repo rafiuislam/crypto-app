fc8f16abcc463772273907238c74795b6070bdfd
unknown <md.rafiu.islam@g.bracu.ac.bd>
2021-11-28 09:08:52 +0600

    basic homepage layout done

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import { Typography, Row, Col, Statistic } from "antd";
+import millify from "millify";
+import { Link } from "react-router-dom";
 
+const { Title } = Typography;
 const Homepage = () => {
-    return <div>Homepage</div>;
+    return (
+        <>
+            <Title level={3} className="heading">
+                Global Crypto Stats
+            </Title>
+            <Row>
+                <Col span={12}>
+                    <Statistic
+                        title="Total Cryptocurrencies"
+                        value="5"
+                    ></Statistic>
+                </Col>
+                <Col span={12}>
+                    <Statistic title="Total Exchanges" value="5"></Statistic>
+                </Col>
+                <Col span={12}>
+                    <Statistic title="Total Market Gap" value="5"></Statistic>
+                </Col>
+                <Col span={12}>
+                    <Statistic title="Total 24h Volume" value="5"></Statistic>
+                </Col>
+                <Col span={12}>
+                    <Statistic title="Total Markets" value="5"></Statistic>
+                </Col>
+            </Row>
+        </>
+    );
 };
 
 export default Homepage;
