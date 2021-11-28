8fe908006b4ffd283c4978988c5c4e35f58dff25
unknown <md.rafiu.islam@g.bracu.ac.bd>
2021-11-28 11:09:35 +0600

    Api fetched from rapidapi by Redux Toolkit

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -2,9 +2,17 @@ import React from "react";
 import { Typography, Row, Col, Statistic } from "antd";
 import millify from "millify";
 import { Link } from "react-router-dom";
+import { useGetCryptosQuery } from "../services/cryptoApi";
 
 const { Title } = Typography;
+
 const Homepage = () => {
+    const { data, isFetching } = useGetCryptosQuery();
+    const globalStats = data?.data?.stats;
+
+    if (isFetching) return "Loading...";
+    console.log(data);
+
     return (
         <>
             <Title level={3} className="heading">
@@ -14,20 +22,32 @@ const Homepage = () => {
                 <Col span={12}>
                     <Statistic
                         title="Total Cryptocurrencies"
-                        value="5"
+                        value={globalStats.total}
                     ></Statistic>
                 </Col>
                 <Col span={12}>
-                    <Statistic title="Total Exchanges" value="5"></Statistic>
+                    <Statistic
+                        title="Total Exchanges"
+                        value={millify(globalStats.totalExchanges)}
+                    ></Statistic>
                 </Col>
                 <Col span={12}>
-                    <Statistic title="Total Market Gap" value="5"></Statistic>
+                    <Statistic
+                        title="Total Market Cap"
+                        value={millify(globalStats.totalMarketCap)}
+                    ></Statistic>
                 </Col>
                 <Col span={12}>
-                    <Statistic title="Total 24h Volume" value="5"></Statistic>
+                    <Statistic
+                        title="Total 24h Volume"
+                        value={millify(globalStats.total24hVolume)}
+                    ></Statistic>
                 </Col>
                 <Col span={12}>
-                    <Statistic title="Total Markets" value="5"></Statistic>
+                    <Statistic
+                        title="Total Markets"
+                        value={millify(globalStats.totalMarkets)}
+                    ></Statistic>
                 </Col>
             </Row>
         </>
